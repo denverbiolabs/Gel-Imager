47f27c18a1b61952340429a49c7f5ff51c615570
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2017-02-04 00:56:28 -0700

    Convert image to black and white

--- src/imagefilter.py
+++ src/imagefilter.py
@@ -12,3 +12,6 @@ if __name__ == "__main__":
         image_base = Image.open(filepath)
     except IOError:
         sys.exit("ERROR: Couldn't open image from %s" % filepath)
+
+    #Convert to 8-bit black and white image mode
+    image_bw = image_base.convert("L")
