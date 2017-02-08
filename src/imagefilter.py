1bea05fc47051d5823000fdb95dbcd82a701afc3
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2017-02-07 19:20:47 -0700

    Detect threshold value via pixel histogram

--- src/imagefilter.py
+++ src/imagefilter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
 
+import numpy as np
+from scipy.signal import argrelmin
 from PIL import Image
 
 if __name__ == "__main__":
@@ -16,7 +18,15 @@ if __name__ == "__main__":
     #Convert to 8-bit black and white image mode
     image_bw = image_base.convert("L")
 
+    #find local minima
+    hist = image_bw.histogram()
+    minima = argrelmin(np.array(hist), order=10)[0]
+    threshold = minima[0] #Current assumption: Because of the way argrelmin clips
+    #values, the left side of the histogram won't actually count as a local minimum,
+    #so the first local minimum will be the center of the valley between the gel color
+    #and the band color.
+    #I'm not totally sure if this is generally true.
+
     #Set all shades to black below a given threshold
-    threshold = 100
     image_filtered = image_bw.point(lambda p: 0 if p < threshold else p)
     image_filtered.save("test_out.png")
